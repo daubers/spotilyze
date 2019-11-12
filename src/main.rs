539123b431e05c7f312a5754cf977674761ccdb6
Matthew Daubney <matt@daubers.co.uk>
2019-11-12 20:45:07 +0000

    Starting playing with jql over rocket

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 
+#![feature(decl_macro, proc_macro_hygiene)]
+
 mod spotify;
 
 #[macro_use]
 extern crate diesel;
+
+use rocket::{response::content, State};
+
+use juniper::{
+    tests::{model::Database, schema::Query},
+    EmptyMutation, RootNode,
+};
 
 pub mod schema;
 pub mod models;
@@ -10,7 +19,42 @@ pub mod db;
 
 use crate::spotify::main as spotimain;
 
+
+
+type Schema = RootNode<'static, Query, EmptyMutation<Database>>;
+
+#[rocket::get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::graphiql_source("/graphql")
+}
+
+#[rocket::get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
 fn main() {
     println!("Hello, world!");
     spotimain();
+    rocket::ignite()
+        .manage(Database::new())
+        .manage(Schema::new(Query, EmptyMutation::<Database>::new()))
+        .mount(
+            "/",
+            rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
+        .launch();
 }
